0704a753022c8b5a70092d36a48225a8c2b95f0d
jiangjiaqi <3110103518@zju.edu.cn>
2024-10-22 00:41:36 +0800

    feat: Base Function

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { alphaTab } from "@coderline/alphatab/vite";
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    alphaTab({
+      alphaTabSourceDir: path.resolve('./node_modules/@coderline/alphatab/dist'),
+    })
+  ],
 })
